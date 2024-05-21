4fb6d3210e008354ed90f4a0c782fb79e8127746
alpdk <alexander@podkopaev.net>
2024-05-21 15:55:11 +0300

    Added voice cloning option

--- Jarvis/text_to_speech.py
+++ Jarvis/text_to_speech.py
@@ -1,20 +1,33 @@
 import os
 import pathlib
+import replicate
 
 from gtts import gTTS
 from playsound import playsound
 
-def play_text_to_speech(text, language = 'en'):
-    myobj = gTTS(text=text, lang=language, slow=False)
+os.environ["'REPLICATE_API_TOKEN'"] = os.getenv('REPLICATE_API_TOKEN')
 
-    path_to_proj_dir = pathlib.Path().resolve()
-    path_to_mp3_anser = os.path.join(path_to_proj_dir, "answer.mp3")
 
-    myobj.save(path_to_mp3_anser)
+def assistent_speak(text, language="en",
+                    reference_voice="https://www.myinstants.com/en/instant/vergil-i-need-more-power-43083/?utm_source=copy&utm_medium=share"):
+    output = replicate.run(
+        "lucataco/xtts-v2:684bc3855b37866c0c65add2ff39c78f3dea3f4ff103a436465326e0f438d55e",
+        input={
+            "text": text,
+            "speaker": reference_voice,
+            "language": language,
+            "cleanup_voice": False
+        }
+    )
 
-    playsound(path_to_mp3_anser)
+    return output
 
-    os.remove(path_to_mp3_anser)
+
+def play_text_to_speech(text, language='en'):
+    sound_ref = assistent_speak(text, language)
+
+    playsound(sound_ref)
+
 
 if __name__ == '__main__':
-    play_text_to_speech("aboba")
+    play_text_to_speech("I am a son of sparda")
